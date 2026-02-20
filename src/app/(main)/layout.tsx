5591949d64629b5118f9dad74b783966ecf6e2c5
Marycarmen <marycarmen.rivo@gmail.com>
2026-02-20 16:15:12 -0600

    Created basic navbar, footer and logout button components

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 export default function MainLayout({
   children,
 }: {
@@ -5,9 +8,9 @@ export default function MainLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <nav>Navbar</nav>
+      <Navbar />
       <main className="grow container mx-auto px-4 py-8">{children}</main>
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import LogoutButton from "./LogoutButton";
+
+export default function Navbar() {
+  return (
+    <nav>
+      Navbar <LogoutButton />
+    </nav>
+  );
+}
