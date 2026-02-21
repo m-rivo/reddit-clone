5e6d398ddaec23946141a5696d72dab7a79616c5
Marycarmen <marycarmen.rivo@gmail.com>
2026-02-20 20:40:25 -0600

    Created logout button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,5 +1,25 @@
+"use client";
+
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { pb } from "@/lib/pocketbase";
+import Cookies from "js-cookie";
+import { LogOut } from "lucide-react";
 
 export default function LogoutButton() {
-  return <Button>Logout</Button>;
+  const router = useRouter();
+
+  const handleLogout = () => {
+    pb.authStore.clear(); // client
+    Cookies.remove("pb_auth"); // server
+
+    router.push("/login");
+    router.refresh();
+  };
+
+  return (
+    <Button onClick={handleLogout} variant="destructive">
+      Logout <LogOut />
+    </Button>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import LogoutButton from "./LogoutButton";
+import { cookies } from "next/headers";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const cookieStore = await cookies();
+  const authCookie = cookieStore.get("pb_auth");
+
+  const isLogged = !!authCookie?.value;
+
   return (
     <nav>
-      Navbar <LogoutButton />
+      <span>Reddit Clone</span>
+      {isLogged && <LogoutButton />}
     </nav>
   );
 }
